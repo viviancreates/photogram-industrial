ab682656c60f8d94fd6a09f2265cf98c7373c258
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-10-14 19:50:44 +0000

    Added direct associations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -21,4 +21,8 @@
 #
 class Photo < ApplicationRecord
   belongs_to :owner, class_name: "User", counter_cache: true
+
+  has_many :comments
+
+  has_many :likes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -29,4 +29,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :comments, foreign_key: :author_id
+
+  has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequests"
+
+  has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequests"
+
+  has_many :likes, foreign_key: :fan_id 
+
+  has_many :own_phots, foreign_key: :owner_id, class_name: "Photo"
 end
