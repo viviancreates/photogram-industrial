7656c9621149156519a44e926a85c5f9a506c2bf
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-10-14 20:27:46 +0000

    Added indirect associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,11 +34,23 @@ class User < ApplicationRecord
 
   has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequests"
 
+   has_many :accepted_sent_follow_requests, -> { where(status: "accepted") }, foreign_key: :sender_id, class_name: "FollowRequest"
+
   has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequests"
+
+has_many :accepted_reveived_follow_requests, -> { where(status: "accepted") }, foreign_key: :recipient_id, class_name: "FollowRequest"
 
   has_many :likes, foreign_key: :fan_id 
 
   has_many :own_phots, foreign_key: :owner_id, class_name: "Photo"
 
-  has_many :liked_photos, through: :likes
+  has_many :liked_photos, through: :likes, source: :photo
+
+  has_many :leaders, through: :accepted_sent_follow_requests, source: :recipient
+
+  has_many :followers, through: :accepted_received_follow_requests, source: :sender
+
+  has_many :feed, through: :leaders, source: :own_photos
+
+  has_many :discover, through: :leaders, source: :liked_photos
 end
