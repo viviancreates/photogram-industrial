02f6a04e8e81f212cb56c369c4b02ca8829c261d
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-10-16 16:55:07 +0000

    User route is working

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   resources :follow_requests
   resources :likes
   resources :photos
-  
+
+  get "/:username" => "users#show", as: :user
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
