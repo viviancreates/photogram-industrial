383db82dc8c4b0859be2dd16562fdb2dca5c9150
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-10-16 21:28:31 +0000

    Finished photo commenting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -26,7 +26,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to comment_url(@comment), notice: "Comment was successfully created." }
+        format.html { redirect_back fallback_location: root_path, notice: "Comment was successfully created." }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new, status: :unprocessable_entity }
